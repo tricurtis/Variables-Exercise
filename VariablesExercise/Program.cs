e40a8b403ce1cc484abc2c64476c1758f04834b1
Trigail Curtis <trigailcurtisabm@gmail.com>
2023-01-23 19:38:03 -0600

    intitial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declare and inintialize variables
+string name = "John";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+
+// Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($" Current temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
